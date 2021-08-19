d9a3e2dc803996805348631716bed3bd0d6dbca9
ryand113 <kevinsanryandsie@gmail.com>
2021-08-19 11:26:11 +0700

    fingerprint/goodix/gf_spi.c: silence log spam
    Signed-off-by: ryand113 <kevinsanryandsie@gmail.com>

--- drivers/input/fingerprint/goodix_ta_vayu/gf_spi.c
+++ drivers/input/fingerprint/goodix_ta_vayu/gf_spi.c
@@ -347,9 +347,6 @@ static void gf_kernel_key_input(struct gf_dev *gf_dev, struct gf_key *gf_key)
 		key_input = gf_key->key;
 	}
 
-	pr_info("%s: received key event[%d], key=%d, value=%d\n",
-		 __func__, key_input, gf_key->key, gf_key->value);
-
 	if ((GF_KEY_POWER == gf_key->key || GF_KEY_CAMERA == gf_key->key)
 		&& (gf_key->value == 1)) {
 		input_report_key(gf_dev->input, key_input, 1);
